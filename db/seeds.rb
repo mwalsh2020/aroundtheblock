462c6f23a19ed904fafdacc570d545a4fb1f0545
Matthew Walsh <mattnwalsh@gmail.com>
2021-05-18 22:56:56 -0700

    more pic edits

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,17 +22,23 @@ file = URI.open('https://res.cloudinary.com/le-wagon21bitcoin/image/upload/v1621
 posting1.photo.attach(io: file, filename: 'solana.png', content_type: 'image/png')
 
 
-Posting.create!(
+posting2 = Posting.create!(
   user: user,
   title: "Blockchain Developer Needed for: SPINWORLD DJ Blockchain Project",
   description: "WE are looking for someone to help us build a fullscale Blockchain platform for DJ's called Spinworld.",
   price: 1700.00
 )
 
-Posting.create!(
+file = URI.open('https://res.cloudinary.com/le-wagon21bitcoin/image/upload/v1621113561/amino/bitcoin_lwhzk1.png')
+posting2.photo.attach(io: file, filename: 'bitcoin.png', content_type: 'image/png')
+
+
+posting3 = Posting.create!(
   user: user,
-  title: "Binance Smart Chain Bot for Sniping BEP20 Tokens & Pre-Launch Coins",
-  description: "I’m looking for someone to create a sniper bot that uses the Binance Smart Chain and detects when new liquidity is added to an AMM (automated market maker) pool on Pancakeswap v1/v2 (runs on the Binance Smart Chain, BEP20 network). It’s paramount the bot is efficient in sacrificing high gas fees to be one of the first orders placed (front-run)The BOT would essentially detect and immediately execute a trade on the same second liquidity to is added to a token, according to my parameters",
+  title: "Ethereum Smart Chain Bot for Sniping BEP20 Tokens & Pre-Launch Coins",
+  description: "I’m looking for someone to create a sniper bot that uses the Ethereum Smart Chain and detects when new liquidity is added to an AMM (automated market maker) pool on Pancakeswap v1/v2 (runs on the Ethereum Smart Chain, BEP20 network). It’s paramount the bot is efficient in sacrificing high gas fees to be one of the first orders placed (front-run)The BOT would essentially detect and immediately execute a trade on the same second liquidity to is added to a token, according to my parameters",
   price: 1000.00
 )
 
+file = URI.open('https://res.cloudinary.com/le-wagon21bitcoin/image/upload/v1621113561/amino/eth_kcm3t5.png')
+posting3.photo.attach(io: file, filename: 'ethereum.png', content_type: 'image/png')
